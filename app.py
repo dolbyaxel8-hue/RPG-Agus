02bd1bee9f398d842323757de63d20eff2a69334
Axel <dolbyaxel8@gmail.com>
2026-08-05 12:35:43 +0200

    creo pantaya web del RPG

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    return "RPG Agus funcionando"
+    return render_template("juego.html")
 
 if __name__ == "__main__":
     app.run()
